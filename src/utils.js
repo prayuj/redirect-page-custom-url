242c4fee33512b9407e1d13f5bbc92ef1f9420cf
Prayuj Pillai <prayuj.pillai@pens.com>
2021-07-29 16:13:18 +0530

    added get cookie

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,25 @@ export const isLoggedIn = () => {
     return Cookies.get('key') !== undefined;
 }
 
+export const getCookie = () => {
+    return Cookies.get('key');
+}
+
 export const setCookie = (value) => {
-    console.log(value)
     Cookies.set('key', value);
+}
+
+export const getAxiosOptions = (endpoint, method, data = {}) => {
+
+    const options = {
+        url: `${process.env.REACT_APP_CUSTOM_URL_ENDPOINT}/${endpoint}`,
+        method,
+        data,
+        headers: {
+            key: getCookie()
+        },
+        withCredentials: true
+    }
+
+    return options;
 }
